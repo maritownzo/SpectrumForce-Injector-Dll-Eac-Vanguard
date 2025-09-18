e57fd2610b6327cc0447562afc7c21273195c757
slit2468 <paying1965@outlook.com>
2025-09-18 12:00:22 +0400

    Add resource.h

---
+++ SpectrumForceInjector/resource.h
@@ -0,0 +1,45 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by DownsInjector.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_DOWNSINJECTOR_DIALOG        102
+#define IDS_STRING102                   102
+#define IDS_STRING103                   103
+#define IDS_STRING104                   104
+#define IDS_STRING105                   105
+#define IDR_MAINFRAME                   128
+#define IDC_BUTTON1                     1000
+#define IDC_EDIT1                       1001
+#define IDC_RADIO1                      1002
+#define IDC_RADIO2                      1003
+#define IDC_LOADLIB                     1003
+#define IDC_CHECK1                      1004
+#define IDC_CLOSE_CHECK                 1004
+#define IDC_STATIC_STATUS_MSG           1005
+#define IDC_HANDLE_ASAP                 1006
+#define IDC_CHOSE_FILE_BTN              1007
+#define IDC_CHOSE_FILE_EDIT             1008
+#define IDC_STATIC_MANUAL_OPTIONS       1012
+#define IDC_CHECK_MAP_SXS               1013
+#define IDC_STATIC_PROCESS_SELECTION    1014
+#define IDC_CHECK_MAP_EXCEPTION         1015
+#define IDC_CHECK_MAP_SXS2              1016
+#define IDC_CHECK_MAP_THREAD_HIJACK     1016
+#define IDC_SELECT_WINDOW               1017
+#define IDC_SELECT_PROCESS_LIST         1018
+#define IDC_SELECT_PROCESS_FILE         1019
+#define IDC_ADITIONAL_OPTIONS           1020
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1021
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
